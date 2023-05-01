123163e553125e3228210234296851ff1e38700b
lobinhodev <contatolobinhodev@gmail.com>
2023-04-30 23:37:35 -0300

    init: commit initial

---
+++ File/Program.cs
@@ -0,0 +1,40 @@
+﻿var folderName = "pasta";
+var subFolderName = "subpasta";
+var subFolderNameStatic = "subpasta_usingStatic";
+
+var directoryInfoSubFolder = new DirectoryInfo(subFolderName);
+
+if (!Directory.Exists(subFolderName) || !directoryInfoSubFolder.Exists)
+{
+    Directory.CreateDirectory(subFolderNameStatic);
+    Directory.CreateDirectory(folderName);
+    directoryInfoSubFolder.Create();
+
+    directoryInfoSubFolder.MoveTo($"{folderName}//{subFolderName}");
+    Directory.Move(subFolderNameStatic, $@"{folderName}/{subFolderNameStatic}");
+}
+
+var name = directoryInfoSubFolder.Name;
+var parent = directoryInfoSubFolder.Parent;
+var root = directoryInfoSubFolder.Root;
+var exists = directoryInfoSubFolder.Exists;
+
+foreach (var directory in Directory.GetDirectories(folderName))
+{
+    Console.WriteLine(directory);
+}
+
+Directory.Delete($@"{folderName}/{subFolderName}");
+
+//file and archive
+
+var file = $"pasta/text.txt";
+
+if (!File.Exists(file))
+{
+    File.Create(file);
+}
+
+var fileInfo = new FileInfo(file);
+
+Console.WriteLine($"Nome: {fileInfo.Name}, Tamanho: {fileInfo.Length}, Data de atualização: {fileInfo.LastWriteTime}.");
